c9d5dd911d0a360a764fc3ea79acf75b369e0177
ryota-ko <trb_right9@yahoo.co.jp>
2016-11-08 11:56:29 +0900

    modify GoogleApiClient to HTTPURLConnection

--- app/src/main/java/com/example/koshibaryouta/marketstop/MainActivity.java
+++ app/src/main/java/com/example/koshibaryouta/marketstop/MainActivity.java
@@ -7,77 +7,53 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.location.places.PlaceLikelihood;
-import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
-import com.google.android.gms.location.places.Places;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity
-        extends AppCompatActivity
-        implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
-
-    private GoogleApiClient mGoogleApiClient;
-    StringBuilder url = new StringBuilder("https://maps.googleapis.com/maps/api/place/search/json");
+        extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mGoogleApiClient = new GoogleApiClient
-                .Builder(this)
-                .addApi(Places.GEO_DATA_API)
-                .addApi(Places.PLACE_DETECTION_API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    protected void onStop() {
-        mGoogleApiClient.disconnect();
-        super.onStop();
-    }
-
-    //接続成功時に呼ばれる
-    @Override
-    public void onConnected(Bundle bundle) {
-
-        Log.v("接続", "Connected");
-        PendingResult<PlaceLikelihoodBuffer> result = null;
-        try {
-            result = Places.PlaceDetectionApi.getCurrentPlace( mGoogleApiClient , null );
-        } catch (SecurityException e) {
-            Log.e("PERMISSION_EXCEPTION","PERMISSION_NOT_GRANTED");
-        }
-        result.setResultCallback( new ResultCallback<PlaceLikelihoodBuffer>() {
+        final StringBuilder urlString = new StringBuilder();
+        urlString.append("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        urlString.append("location=34.6682532,135.6180929");
+        urlString.append("&radius=500");
+        urlString.append("&types=grocery_or_supermarket");
+        urlString.append("&key=AIzaSyBVBUGCXX4Ot7z8CP4ynTkWXWffDF9QB2k");
+        new Thread(new Runnable() {
             @Override
-            public void onResult( PlaceLikelihoodBuffer likelyPlaces ) {
-                for ( PlaceLikelihood placeLikelihood : likelyPlaces ) {
-                    Log.i("PickerTest", String.format( "Place '%s' has likelihood: %g",
-                            placeLikelihood.getPlace().getName(),
-                            placeLikelihood.getLikelihood()) );
+            public void run() {
+                try {
+                    // 大阪の天気予報XMLデータ
+                    URL url = new URL(new String(urlString));
+                    HttpURLConnection con = (HttpURLConnection)url.openConnection();
+                    con.connect();
+                    String str = InputStreamToString(con.getInputStream());
+                    Log.d("HTTP", str);
+                } catch(Exception ex) {
+                    Log.d("ERROR", "" + ex);
                 }
-                likelyPlaces.release();
             }
-        });
+        }).start();
     }
 
-    // 接続が中断された時に呼ばれる。原因は引数で渡される
-    @Override
-    public void onConnectionSuspended(int i) {Log.d("Test", "中断");}
-
-    // 接続失敗時に呼ばれる。
-    @Override
-    public void onConnectionFailed(ConnectionResult result) {Log.d("Test", "失敗");}
-
+    // InputStream -> String
+    static String InputStreamToString(InputStream is) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+        }
+        br.close();
+        return sb.toString();
+    }
 }
